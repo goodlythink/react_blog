2c04d204dcd8b5832afc39f38258237c9466c560
GoodlyThink <goodlythink@gmail.com>
2017-10-05 10:29:37 +0700

    edit webpack

--- web/index.js
+++ web/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
-    return (
-        <div>Hi React 2</div>
-    )
+class App extends React.Component {
+    render() {
+        return (
+            <div>Hi React 2</div>
+        )
+    }    
 }
 
 ReactDOM.render(
