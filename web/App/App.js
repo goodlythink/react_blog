17eee965a38190b41e47ee94f029a5bdc1181970
GoodlyThink <goodlythink@gmail.com>
2017-10-06 22:37:50 +0700

    react router 3

--- web/App/App.js
+++ web/App/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Header from 'Header/Header'
 import css from './App.module.scss'
+import { Link } from 'react-router'
 
 class App extends React.Component {
     render() {
         return (
             <div className={css.container}>
-               <Header/>
-               <div className={css.content}>
-                    Content 2
+                <Header />
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+                <div className={css.content}>
+                    {this.props.children}
                 </div>
             </div>
         )
--- web/index.js
+++ web/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss';
-import App from './App/App'
+
+import { Router, browserHistory } from 'react-router'
+import routes from './routes'
 
 ReactDOM.render(
-    <App />,
+    <Router
+        history={browserHistory}
+        routes={routes}
+    />,
     document.getElementById('app')
 )
