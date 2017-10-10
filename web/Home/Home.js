f604240d3123581ae2b4ec1ad6ff7c4a33915144
GoodlyThink <goodlythink@gmail.com>
2017-10-10 11:11:16 +0700

    Redux Middleware Promise

--- web/Home/Home.js
+++ web/Home/Home.js
@@ -1,26 +1,45 @@
 import React from 'react'
+import { connect } from 'react-redux';
 import PostList from '../Posts/PostList'
 
 class Home extends React.Component {
-    state = { data: null, isLoading: false }
-
     componentDidMount() {
-        this.setState({ isLoading: true })
-        fetch('http://jsonplaceholder.typicode.com/posts?userId=1')
-            .then(d => d.json())
-            .then(d => this.setState({ data: d, isLoading: false }))
+        // this.setState({ isLoading: true })
+        // fetch('http://jsonplaceholder.typicode.com/posts?userId=1')
+        //     .then(d => d.json())
+        //     .then(d => this.setState({ data: d, isLoading: false }))
+        this.props.loadPosts()
     }
     render() {
-        const { isLoading, data } = this.state
-
+        const { posts } = this.props
         return (
             <div>
                 <h1>Lastest Posts</h1>
-                {isLoading && <div>Loading...</div>}
-                <PostList data={data}/>
+                <PostList data={posts.data} />
             </div>
         )
     }
 }
 
-export default Home
+function selector(state) {
+    return {
+        posts: state.posts,
+    }
+}
+
+function mapDispatch(dispatch) {
+    return {
+        loadPosts: () => {
+            dispatch({
+                type: 'LOAD_POSTS',
+                payload: fetch('http://jsonplaceholder.typicode.com/posts?userId=1')
+                    .then(d => d.json())
+            })
+        }
+    }
+}
+
+export default connect(
+    selector,
+    mapDispatch,
+)(Home)
